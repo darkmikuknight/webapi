db2e014c0596d842aa5ad6a6f8c7b83f3fdfa389
darkmikuknight <guilhemerps94@gmail.com>
2020-04-12 13:53:35 -0300

    novas_alteracoes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 3000
-const fs = require('fs') 
+//const port = 3000
+const fs = require('fs')
+//const textoCorrecao = undefined
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -11,7 +12,26 @@ const router = express.Router()
 
 router.get('/', (req, res) => res.json ({ messagem: 'Web API funcionando!' }))
 
-function readText (request, response) {
+function correcao (textoCorrecao){
+    console.log(typeof textoCorrecao)
+
+    // fs.writeFile('meuarquivo.txt', JSON.stringify(textoCorrecao), function(erro) {
+    //     if(erro) {
+    //         throw erro;
+    //     }
+    //     console.log("Arquivo salvo");
+    // }); 
+    //return response.status(200).send('Deu')
+
+    textoCorrecao.forEach(function(item, idx){
+        console.log(idx, item)
+    });
+
+
+    
+}
+
+function lerArquivo (request, response) {
     //realizar a leitura de um arquivo no disco
     fs.readFile('files/teste.json', 'utf8', function(err, data) {
         if (err) {
@@ -22,12 +42,10 @@ function readText (request, response) {
 
         console.log(response.statusCode)
         // console.log(data)
-        let texto = JSON.parse(data.toString())
+         const textoCorrecao = JSON.parse(data.toString())
         //let res = texto.split(" ")
-        let teste = texto.chave
-        console.log(texto.chave[0].id) // teste de commit
-
-    
+        let teste = textoCorrecao.chave
+        console.log(textoCorrecao.chave[0].id) 
 
         //var query = require('url').parse(request.url, true).query
         //var id = query.id
@@ -35,18 +53,33 @@ function readText (request, response) {
         console.log('com params=' + request.params.id) //pega o /:id da url 
 
         response.write(data)
-        response.end();
+        correcao(textoCorrecao)
+        response.end()
         console.log('leu arquivo')
-        return teste
+      
+       // return teste
     });
-  }
+}
 
 app.use('/', router)
-app.use('/correcoes/:id', readText)
+app.use('/correcoes/proxima/', lerArquivo)
+//app.use('/correcoes/', lerArquivo)
+
 app.use('*', (req, res) => {
     res.status(404).send({messagem: "Página não encontrada!"})
 })
 
-app.listen(port)
+module.exports = app
+//app.listen(port)
 console.log('API funcionando')
 
+
+
+// const express = require('express')
+// const app = express()
+
+// app.use((req, res) => {
+//         res.status(200).send({messagem: "Págifgfgdgfna não encontrada!"})
+//     })
+
+//     module.exports = app
